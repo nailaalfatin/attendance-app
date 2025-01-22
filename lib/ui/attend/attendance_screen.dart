243b94c81785550dbfc9a371b9e4e44e1cdbda12
nailaalfatin <nailaalfatin@gmail.com>
2025-01-22 10:26:41 +0700

    creating camera box

--- lib/ui/attend/attendance_screen.dart
+++ lib/ui/attend/attendance_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AttendanceScreen extends StatelessWidget {
+class AttendanceScreen extends StatefulWidget {
   const AttendanceScreen({super.key});
 
+  @override
+  State<AttendanceScreen> createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
