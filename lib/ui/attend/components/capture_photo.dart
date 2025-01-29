cb332647aec66b46b30bd0dba33a3882ceb0dc6d
nailaalfatin <nailaalfatin@gmail.com>
2025-01-29 10:43:56 +0700

    make a name input controller component

--- lib/ui/attend/components/capture_photo.dart
+++ lib/ui/attend/components/capture_photo.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:attendance_app/ui/attend/camera_screen.dart';
 import 'package:camera/camera.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 Padding buildCapturePhotoSection(BuildContext context, Size size, XFile? image) {
@@ -23,6 +26,28 @@ Padding buildCapturePhotoSection(BuildContext context, Size size, XFile? image) 
               MaterialPageRoute(builder: (context) => const CameraScreen())
             );
           },
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+            width: size.width,
+            height: 150,
+            child: DottedBorder(
+              radius: const Radius.circular(10),
+              borderType: BorderType.RRect,
+              color: Colors.blueAccent,
+              strokeWidth: 1,
+              dashPattern: const [5, 5],
+              child: SizedBox.expand(
+                child: FittedBox(
+                  child: image != null
+                  ? Image.file(File(image.path), fit: BoxFit.cover)
+                  : const Icon(
+                    Icons.camera_enhance_outlined,
+                    color: Colors.blueAccent
+                  )
+                )
+              )
+            ),
+          ),
         )
       ],
     ),
