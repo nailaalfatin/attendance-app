2f32593dadfd722853d20f172dfbe30dbbf8144a
nailaalfatin <nailaalfatin@gmail.com>
2025-01-29 09:41:59 +0700

    make an appbar component

--- lib/ui/attend/attendance_screen.dart
+++ lib/ui/attend/attendance_screen.dart
@@ -1,15 +1,12 @@
-import 'dart:io';
-
-import 'package:attendance_app/ui/attend/camera_screen.dart';
+import 'package:attendance_app/services/location_service.dart';
+import 'package:attendance_app/services/timestamp_service.dart';
 import 'package:camera/camera.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AttendanceScreen extends StatefulWidget {
-  final XFile? image; //dikasih ? biar null safety
+  final XFile? image;
 
-  const AttendanceScreen({super.key, this.image});
+  const AttendanceScreen({super.key, required this.image});
 
   @override
   State<AttendanceScreen> createState() => _AttendanceScreenState(this.image);
@@ -19,153 +16,44 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
   _AttendanceScreenState(this.image);
 
   XFile? image;
-  String strAddress = "", date = "", time = "", dateTime = "", status = "Attend";
+  String addressPlaceholder = '', datePlaceholder = '', timePlaceholder = '', timeStampPlaceholder = '', statusPlaceholder = 'Attend';
   bool isLoading = false;
-  double lat = 0.0, long = 0.0; //detail location 
-  int dateHours = 0, minute = 0;
-  final controller = TextEditingController();
-  final CollectionReference dataCollection = FirebaseFirestore.instance.collection('attendance'); //'attendance' -> ini itu key nya, sama aja kaya waktu di mysql
+  final controllerName = TextEditingController();
 
   @override
   void initState() {
-    // handleLocationPermission();
-    // setDateTime();
-    // setAttendStatus();
+    super.initState();
+    handleLocationPermission(context);
+    setDateTime((date, time, timeStamp) {
+      setState(() {
+        datePlaceholder = date;
+        timePlaceholder = time;
+        timeStampPlaceholder = timeStamp;
+      });
+    });
+    setAttendanceStatus((status) {
+      setState(() {
+        statusPlaceholder = status;
+      });
+    });
 
-    // ketika image itu ada isinya
     if (image != null) {
       isLoading = true;
-      // getGeoLocationPosition();
+      getGeoLocationPosition(context, (position) {
+        setState(() { // set sebelum ada perubahan data
+          isLoading = false;
+          getAddressFromLongLat(position, (address) {
+            setState(() {
+              addressPlaceholder = address;
+            });
+          });
+        });
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.blueAccent,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () => Navigator.of(context).pop()
-        ),
-        title: const Text(
-          "Attendance Report",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Card(
-          color: Colors.white,
-          margin: const EdgeInsets.fromLTRB(10, 10, 10, 30),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10)
-          ),
-          elevation: 5,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 10,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10)
-                  ),
-                  color: Colors.blueAccent
-                ),
-                child: const Row(
-                  children: [
-                    SizedBox(width: 12),
-                    Icon(Icons.face_retouching_natural_outlined),
-                    SizedBox(width: 12),
-                    Text(
-                      "Please scan your face!",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(10, 20, 0, 20),
-                child: Text(
-                  "Capture Image",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const CameraScreen())
-                  );
-                },
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-                  width: size.width,
-                  height: 150,
-                  child: DottedBorder(
-                    radius: const Radius.circular(10),
-                    borderType: BorderType.Rect,
-                    color: Colors.blueAccent,
-                    strokeWidth: 1,
-                    dashPattern: const [5,3],
-                    child: SizedBox.expand(
-                      child: FittedBox(
-                        child: image != null
-                        ? Image.file(File(image!.path), fit: BoxFit.cover)
-                        : const Icon(Icons.camera_enhance_outlined)
-                      ),
-                    )
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.text,
-                  controller: controller,
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 10),
-                    labelText: "Your Name",
-                    hintText: "Please type name here",
-                    hintStyle: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey
-                    ),
-                    labelStyle: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.black
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.blueAccent)
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.blueAccent)
-                    )
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
